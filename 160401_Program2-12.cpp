501ddeafe5f8a6528f090cde70dd0de79197ca94
ChuangMandy <mandy840907@gmail.com>
2016-04-01 20:29:18 -0700

    Program 2-9 - 2-17

---
+++ 160401_Program2-12.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//
+//    char letter;
+//
+//    letter = 'A';
+//    cout << letter << endl;
+//    letter = 'B';
+//    cout << letter << endl;
+//    return 0;
+//}
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    char letter;
+
+    letter = 48;
+    cout << letter << endl;
+    letter = 97;
+    cout << letter << endl;
+    return 0;
+}
