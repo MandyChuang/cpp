a9f00f544654fa8bd519d7883be598632c39d420
ChuangMandy <mandy840907@gmail.com>
2016-10-08 21:42:59 -0700

    160401 textbook problems

---
+++ Textbook_Problems/160401_Program2-16.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float distance;
+    double mass;
+    long long int d;
+
+    distance = -1.7;
+    d = distance;
+    mass = 1.989E30;
+
+    cout << d << endl;
+    cout << "The Sun is " << distance << " meters away.\n";
+    cout << "The Sun\'s mass is " << mass << " kilograms.\n";
+    return 0;
+}
---
+++ Textbook_Problems/160401_Program2-6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "The following items were top sellers\n";
+    cout << "during the\" month \tof June:\n";
+    cout << "Computer\' games\nCoffee";
+    cout << "\nAspirin\n";
+    cout<<"12\t3"<<endl;
+    cout<<"4\t56"<<endl;
+    return 0;
+}
